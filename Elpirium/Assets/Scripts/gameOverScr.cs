4ee16d3e78000a5e4a0f5f7589f56651ae4f4706
dengol04 <dengol04@mail.ru>
2024-03-31 01:52:57 +0300

    dialogs fixing

--- Elpirium/Assets/Scripts/gameOverScr.cs
+++ Elpirium/Assets/Scripts/gameOverScr.cs
@@ -23,12 +23,12 @@ public class gameOverScr : MonoBehaviour
         Debug.Log("Current level <= Levels Amount: " + (PlayerPrefs.GetInt("currentLevel") <= LevelManager.levelsAmount));
         if (PlayerPrefs.GetInt("currentLevel") > LevelManager.levelsAmount)
         {
-            GameObject.Find("nextLevelButton").GetComponent<Button>().enabled = false;
+            GameObject.Find("nextLevelButton").GetComponent<Button>().interactable = false;
             int currLev = PlayerPrefs.GetInt("currentLevel");
             PlayerPrefs.SetInt("currentLevel", --currLev);
         }
         else
-            GameObject.Find("nextLevelButton").GetComponent<Button>().enabled = true;
+            GameObject.Find("nextLevelButton").GetComponent<Button>().interactable = true;
     }
 
     public void loadPrevLevel()
